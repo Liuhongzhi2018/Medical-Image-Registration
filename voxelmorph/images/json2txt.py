63720e00c270846e99dd010119a891a17876e7b6
Liuhongzhi2018 <liuhongzhi_006@163.com>
2024-08-02 23:58:27 +0800

    Upload new files

---
+++ voxelmorph/images/json2txt.py
@@ -0,0 +1,21 @@
+import os
+import numpy
+import numpy as np
+import json
+
+def json2txt(json_file, txt_file):
+    
+    with open(json_file,'r',encoding='utf-8') as load_f:
+        load_dict = json.load(load_f)
+    
+    # print(f"json: {load_dict} {load_dict[0]}")
+    with open(txt_file,'w') as f:
+        for item in load_dict:
+            line = item["image_ED"] + " " + item['image_ES'] + "\n"
+            print(f"line: {line}")
+            f.write(line)
+
+if __name__ == "__main__":
+    json_file = "/home/liuhongzhi/Method/Registration/voxelmorph/images/ACDC/test.json"
+    txt_file = "/home/liuhongzhi/Method/Registration/voxelmorph/images/ACDC/test_list.txt"
+    json2txt(json_file, txt_file)
